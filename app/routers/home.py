96f7cdbf28d3bfc257759de1392778f1f1442a21
elyar <elyar.s@bit24.info>
2025-06-18 16:33:01 +0330

    update home route

--- app/routers/home.py
+++ app/routers/home.py
@@ -1,11 +1,15 @@
 from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
 from pathlib import Path
+from app.routers.room import room_data
 
 router = APIRouter()
 templates = Jinja2Templates(directory=str(Path(__file__).resolve().parent.parent / "templates"))
 
 @router.get("/")
 async def get_home(request: Request):
-    rooms = ["A", "B", "C"]
-    return templates.TemplateResponse("home.html", {"request": request, "rooms": rooms})
+    rooms = [{"id": room_id, **room_data[room_id]} for room_id in room_data]
+    return templates.TemplateResponse("home.html", {
+        "request": request,
+        "rooms": rooms
+    })
